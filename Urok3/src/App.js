4e075b172379878473bdb70b7894cf90b53688fc
sagadeevaleisan <151964873+sagadeevaleisan@users.noreply.github.com>
2024-02-21 13:40:42 +0500

    Add files via upload

---
+++ Urok3/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Header from './components/Header';
+import Card from './components/Card';
+import Drawer from './components/Drawer';
+
+const arr = [
+  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: '/img/sneakers/1.jpg' },
+  { title: 'Мужские Кроссовки Nike Air Max 270', price: 15600, imageUrl: '/img/sneakers/2.jpg' },
+  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: '/img/sneakers/3.jpg' },
+  { title: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl: '/img/sneakers/4.jpg' },
+];
+
+function App() {
+  return (
+    <div className="wrapper clear">
+      <Drawer />
+      <Header />
+      <div className="content p-40">
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Все кроссовки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Поиск..." />
+          </div>
+        </div>
+
+        <div className='d-flex'>
+          { arr.map((obj) => (
+            <Card
+              title={obj.title}
+              price={obj.price}
+              imageUrl={obj.imageUrl}
+              onClick={() => console.log(obj)}
+            />
+          )) }
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
